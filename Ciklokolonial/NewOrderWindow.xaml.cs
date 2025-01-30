8d657e3165bb5b8082a03bf7bf5d79a8a0317f75
codelamszivotem <144122024+codelamszivotem@users.noreply.github.com>
2025-01-30 08:00:56 +0100

    Add files via upload

---
+++ Ciklokolonial/NewOrderWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Ciklokolonial
+{
+    /// <summary>
+    /// Interakční logika pro NewOrderWindow.xaml
+    /// </summary>
+    public partial class NewOrderWindow : Window
+    {
+
+        public Kolo CreatedOrder { get; private set; }
+
+        public event Action<Kolo> OrderCreated;
+
+        public NewOrderWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Get the selected values from each ComboBox
+            string sedadlo = (comboBox1.SelectedItem as ComboBoxItem)?.Content.ToString();
+            string barva = (comboBox2.SelectedItem as ComboBoxItem)?.Content.ToString();
+            int velikost = int.TryParse((comboBox3.SelectedItem as ComboBoxItem)?.Content.ToString(), out int result) ? result : 0;
+            bool blatniky = (comboBox4.SelectedItem as ComboBoxItem)?.Content.ToString() == "True";
+
+            // Create a new bike object
+            CreatedOrder = new Kolo(sedadlo, barva, velikost, blatniky);
+
+            // Notify the main window about the new order
+            OrderCreated?.Invoke(CreatedOrder);
+
+            // Close the window
+            this.Close();
+        }
+    }
+}
